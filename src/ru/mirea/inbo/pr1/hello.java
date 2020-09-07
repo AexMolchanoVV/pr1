ed7946a88d3d079fe122f878ff770dc22f3ea2a3
lamossus <zhora.polyakov95@mail.ru>
2020-09-07 14:47:26 +0300

    Practicheskay rabota 1

---
+++ src/ru/mirea/inbo/pr1/hello.java
@@ -0,0 +1,31 @@
+package ru.mirea.inbo.pr1;
+import java.lang.*;
+
+public class hello{
+    public static void main(String[] args){
+        dog dog = new dog();
+        System.out.println(dog);
+        dog.turnIntoHumanAge();
+        dog.setAge(10);
+        dog.turnIntoHumanAge();
+
+        System.out.println(" ");
+
+        book book = new book(1400);
+        book.getBookAge();
+        book.setName("Cool book");
+        book.getBookAge();
+        System.out.println(book);
+
+        System.out.println(" ");
+
+        ball ball = new ball(5, "Red");
+        System.out.println(ball);
+        ball.setColour("Green");
+        System.out.println(ball);
+
+    }
+
+
+
+}
